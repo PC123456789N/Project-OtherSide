97e49e26b8552f3527dee6fb06b83fba70b05a1a
Kaio Henrique <kaio.lopes@escolar.ifrn.edu.br>
2026-04-18 20:56:42 -0300

    feat: made pages changeable still no data transfer

--- src/components/main_components/MainPage.jsx
+++ src/components/main_components/MainPage.jsx
@@ -1,9 +1,15 @@
+import Inicial from "../subcomponents/Inicial";
+import Combat from "../subcomponents/combat";
 import Script from "../subcomponents/Script";
+import Music from "../subcomponents/Music";
 
 export default function MainPage( {selectedId} ){
   return (
     <div className="w-full h-full">
+      {selectedId == 1 && (<Inicial />)}
+      {selectedId == 2 && (<Combat />)}
       {selectedId == 3 && (<Script />)}
+      {selectedId == 4 && (<Music />)}
     </div>
   );
 }
---
+++ src/components/subcomponents/Inicial.jsx
@@ -0,0 +1,5 @@
+export default function Inicial(){
+    return(
+        <>Iniciativas</>
+    )
+}
